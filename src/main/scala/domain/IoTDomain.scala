df3c66fd4cb3c945cca1ca3f09d18c706dd41888
allangsaint <allan.gs@gmail.com>
2026-04-02 16:16:25 -0400

    Update IoTDomain.scala

--- src/main/scala/domain/IoTDomain.scala
+++ src/main/scala/domain/IoTDomain.scala
@@ -1,1 +1,18 @@
+package domain
 
+import java.sql.Timestamp
+
+object IoTDomain {
+
+  // Clase para representar los datos de un sensor de humedad del suelo.
+  // Las Case Classes son ideales para modelar datos inmutables en Scala/Spark.
+  final case class SoilMoistureData(sensorId: String, soilMoisture: Double, timestamp: Timestamp)
+
+  // Clase para representar los datos de un sensor de temperatura y humedad.
+  // zoneId es opcional (Option) ya que inicialmente no sabemos a qué zona pertenece el sensor.
+  final case class TemperatureHumidityData(sensorId: String, temperature: Double, humidity: Double, timestamp: Timestamp, zoneId: Option[String] = None)
+
+  // Clase para representar los datos de un sensor de nivel de CO2.
+  final case class CO2Data(sensorId: String, co2Level: Double, timestamp: Timestamp, zoneId: Option[String] = None)
+
+}
