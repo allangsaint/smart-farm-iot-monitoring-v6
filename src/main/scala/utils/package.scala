adaf77c0a2ac091ab387065069b403f9465410c7
allangsaint <allan.gs@gmail.com>
2026-04-02 16:25:29 -0400

    Add utility functions for sensor ID to zone ID mapping
    This package object contains user-defined functions for mapping sensor IDs to zone IDs and adding a zone ID column to temperature and humidity datasets.

---
+++ src/main/scala/utils/package.scala
@@ -0,0 +1,26 @@
+import AppConfig.sensorToZoneMap
+import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{col, udf}
+
+package object utils {
+
+  object DataTransformations {
+    import domain.IoTDomain._
+    
+    /**
+     * UDF (User Defined Function) para mapear un sensorId a un zoneId.
+     * Si el sensor no existe en el mapa de configuración, devuelve "unknown".
+     */
+    val sensorIdToZoneId: UserDefinedFunction = udf((sensorId: String) => sensorToZoneMap.getOrElse(sensorId, "unknown"))
+
+    /**
+     * Añade una columna "zoneId" a un Dataset de temperatura y humedad.
+     * Utiliza la UDF definida arriba para realizar el mapeo.
+     */
+    def addZoneColumn(df: Dataset[TemperatureHumidityData]): DataFrame = {
+      df.withColumn("zoneId", sensorIdToZoneId(col("sensorId")))
+    }
+
+  }
+}
